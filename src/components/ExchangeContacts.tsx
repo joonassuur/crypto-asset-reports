4fc22f80471d39615b078496860f1e915063a785
JoonasSuurvali <joonas.suurvali@concise.ee>
2024-02-22 08:01:56 +0200

    add exchange contact info

---
+++ src/components/ExchangeContacts.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import { List, ListItemIcon } from '@mui/material';
+import ListItem from '@mui/material/ListItem';
+import { Link, Box } from '@mui/material';
+import ListItemText from '@mui/material/ListItemText';
+import {
+  Link as LinkIcon,
+  AccountBalance,
+  ChatBubbleOutline,
+  Twitter,
+} from '@mui/icons-material';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+import { faker } from '@faker-js/faker';
+
+function ExchangeContacts({ exchangeDetails }: { exchangeDetails: any }) {
+  return (
+    <List
+      dense
+      disablePadding
+      sx={{
+        width: '100%',
+        maxWidth: 360,
+      }}
+    >
+      <React.Fragment>
+        <ListItem alignItems="flex-start">
+          <ListItemText
+            primary={
+              <Box display="flex" justifyContent="space-between">
+                <Typography
+                  variant="h5"
+                  display="flex"
+                  alignItems="center"
+                  fontWeight="bold"
+                >
+                  <Avatar
+                    alt={exchangeDetails.name}
+                    src={faker.image.avatar()}
+                    sx={{ marginRight: '12px' }}
+                  />
+                  {exchangeDetails.name}
+                </Typography>
+              </Box>
+            }
+          />
+        </ListItem>
+        <ListItem alignItems="center">
+          <ListItemIcon
+            sx={{ marginTop: 0, fontSize: 14, maxWidth: 'inherit' }}
+          >
+            <LinkIcon />
+          </ListItemIcon>
+          <ListItemText>
+            <Link component={RouterLink} to="#">
+              {`https://www.${exchangeDetails.name}.com/`}
+            </Link>
+          </ListItemText>
+        </ListItem>
+        <ListItem alignItems="center">
+          <ListItemIcon
+            sx={{ marginTop: 0, fontSize: 14, maxWidth: 'inherit' }}
+          >
+            <AccountBalance />
+          </ListItemIcon>
+          <ListItemText>
+            <Link component={RouterLink} to="#">
+              Fees
+            </Link>
+          </ListItemText>
+        </ListItem>
+        <ListItem alignItems="center">
+          <ListItemIcon
+            sx={{ marginTop: 0, fontSize: 14, maxWidth: 'inherit' }}
+          >
+            <ChatBubbleOutline />
+          </ListItemIcon>
+          <ListItemText>
+            <Link component={RouterLink} to="#">
+              Chat
+            </Link>
+          </ListItemText>
+        </ListItem>
+        <ListItem alignItems="center">
+          <ListItemIcon
+            sx={{ marginTop: 0, fontSize: 14, maxWidth: 'inherit' }}
+          >
+            <Twitter />
+          </ListItemIcon>
+          <ListItemText>
+            <Link component={RouterLink} to="#">
+              {`@${exchangeDetails.name}`}
+            </Link>
+          </ListItemText>
+        </ListItem>
+      </React.Fragment>
+    </List>
+  );
+}
+
+export default ExchangeContacts;
--- src/components/ExchangeDetails.tsx
+++ src/components/ExchangeDetails.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Typography, Grid, Card, CardContent } from '@mui/material';
-
+import ExchangeContacts from './ExchangeContacts';
 import { roundToTwoDecimals } from '../utils/helpers';
 import { faker } from '@faker-js/faker';
 import { useParams } from 'react-router-dom';
@@ -22,41 +22,42 @@ function calculateTotalAssets(exchangeAssets: any) {
 function ExchangeDetails({ exchangeDetails, exchangeAssets }: Props) {
   const { id } = useParams();
   return !id ? null : (
-    <Grid container spacing={2}>
-      <Grid item container xs={12} sm={6}>
-        <Card sx={{ width: '100%' }}>
-          <CardContent
-            sx={{
-              display: 'flex',
-              alignItems: 'flex-end',
-            }}
-          >
-            <Grid item xs={12} sm={6}>
-              <Typography variant="subtitle1" fontWeight="bold">
-                Spot Trading Volume(24h)
-              </Typography>
-              <Typography variant="h5">
-                ${roundToTwoDecimals(exchangeDetails.spot_volume_usd)} T
-              </Typography>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <Typography variant="subtitle1" fontWeight="bold">
-                Total assets
-              </Typography>
-              <Typography variant="h5">
-                ${roundToTwoDecimals(calculateTotalAssets(exchangeAssets))}
-              </Typography>
-            </Grid>
-          </CardContent>
-        </Card>
+    <Grid container spacing={2} justifyContent="flex-start">
+      <Grid item container xs flexGrow="0!important" alignItems="flex-start">
+        <ExchangeContacts exchangeDetails={exchangeDetails} />
       </Grid>
-      <Grid item xs={8}>
-        <Typography variant="h6">About {exchangeDetails.name}</Typography>
-        <Typography variant="body1">{faker.lorem.paragraphs(5)}</Typography>
-        <Typography variant="h6" mt="12px">
-          Who are {exchangeDetails.name} founders?
-        </Typography>
-        <Typography variant="body1">{faker.lorem.paragraphs(3)}</Typography>
+      <Grid item container xs={12} lg={8}>
+        <Grid item container xs={12}>
+          <Card sx={{ width: '100%' }}>
+            <CardContent
+              sx={{
+                display: 'flex',
+                alignItems: 'flex-end',
+              }}
+            >
+              <Grid item xs={12} sm={6}>
+                <Typography variant="h6">Spot Trading Volume(24h)</Typography>
+                <Typography variant="h5" fontWeight="bold">
+                  ${roundToTwoDecimals(exchangeDetails.spot_volume_usd)} T
+                </Typography>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Typography variant="h6">Total assets</Typography>
+                <Typography variant="h5" fontWeight="bold">
+                  ${roundToTwoDecimals(calculateTotalAssets(exchangeAssets))}
+                </Typography>
+              </Grid>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h6">About {exchangeDetails.name}</Typography>
+          <Typography variant="body1">{faker.lorem.paragraphs(5)}</Typography>
+          <Typography variant="h6" mt="12px">
+            Who are {exchangeDetails.name} founders?
+          </Typography>
+          <Typography variant="body1">{faker.lorem.paragraphs(3)}</Typography>
+        </Grid>
       </Grid>
     </Grid>
   );
